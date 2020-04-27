b792788302788a27814b6dc4bfadea641f828ebf
Natalia Titova <bnmlucky@gmail.com>
2020-04-27 17:15:49 -0400

    completed the challenge then added the check for the case when user input is not a number

--- js/random.js
+++ js/random.js
@@ -1,11 +1,18 @@
 // Collect input from a user
-
+let input = prompt("Please enter a number, so that we can generate a random number from 0 to the one you entered.");
 
 // Convert the input to a number
+input = parseInt(input);
 
 
-// Use Math.random() and the user's number to generate a random number
 
+if (input) {
+    // Use Math.random() and the user's number to generate a random number
+    const randomNum = Math.floor(Math.random() * input) + 1;
 
-// Create a message displaying the random number
+    // Create a message displaying the random number
+    alert(`Here is your ${randomNum} between 1 and ${input}`);
 
+} else {
+    console.log("You need to provide a number. Try again.");
+}
