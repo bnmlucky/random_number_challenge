02b29317ec6a03d2983428d9e62e08613305d696
Natalia Titova <bnmlucky@gmail.com>
2020-04-27 18:40:15 -0400

    added code to generate a random number between two numbers the user gives

--- js/random.js
+++ js/random.js
@@ -1,18 +1,20 @@
 // Collect input from a user
-let input = prompt("Please enter a number, so that we can generate a random number from 0 to the one you entered.");
+const inputLow = prompt('Please provide your lowest number.');
+let inputHigh = prompt('Please provide your highest number.');
 
 // Convert the input to a number
-input = parseInt(input);
+const lowNumber = parseInt(inputLow);
+const highNumber = parseInt(inputHigh);
 
 
 
-if (input) {
+if (lowNumber && highNumber) {
     // Use Math.random() and the user's number to generate a random number
-    const randomNum = Math.floor(Math.random() * input) + 1;
+    const randomNum = Math.floor(Math.random() * (highNumber - lowNumber + 1)) + lowNumber;
 
     // Create a message displaying the random number
-    alert(`Here is your ${randomNum} between 1 and ${input}`);
+    alert(`Here is your ${randomNum} between ${lowNumber} and ${highNumber}`);
 
 } else {
-    console.log("You need to provide a number. Try again.");
+    console.log("You need to provide two numbers. Try again.");
 }
